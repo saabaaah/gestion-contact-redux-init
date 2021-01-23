14adc84e777541c4ed2ba5c5e7ad2aedd2534fcf
Hamou Maamar Sabah <saabaaah@gmail.com>
2021-01-23 22:38:34 +0100

    + 35. recupérer données via redux state

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 
+// importer les element de state
+import {connect} from 'react-redux';
+
 class Contacts extends Component {
   state = {
     contacts: [
-      {
-        id: 1,
-        name: 'Mohamed IDBRAHIM',
-        email: 'idbrahimdev@gmail.com',
-        phone: '0650303315'
-      },
-      {
-        id: 2,
-        name: 'Basma IDBRAHIM',
-        email: 'basma@gmail.com',
-        phone: '0650303316'
-      },
-      {
-        id: 3,
-        name: 'Walid IDBRAHIM',
-        email: 'walid@gmail.com',
-        phone: '0650303317'
-      }
+      // {
+      //   id: 1,
+      //   name: 'Mohamed IDBRAHIM',
+      //   email: 'idbrahimdev@gmail.com',
+      //   phone: '0650303315'
+      // },
+      // {
+      //   id: 2,
+      //   name: 'Basma IDBRAHIM',
+      //   email: 'basma@gmail.com',
+      //   phone: '0650303316'
+      // },
+      // {
+      //   id: 3,
+      //   name: 'Walid IDBRAHIM',
+      //   email: 'walid@gmail.com',
+      //   phone: '0650303317'
+      // }
     ]
   };
 
+  // fonction de chargeent du componenet
+  componentDidMount(){
+    this.props.getContacts();
+  }
+
   render() {
-    const { contacts } = this.state;
+    const { contacts } = this.props;
     return (
       <React.Fragment>
         <h1 className="display-4 mb-2">
@@ -40,4 +48,20 @@ class Contacts extends Component {
   }
 }
 
-export default Contacts;
+// recupérer le state
+const mapStateToProps = (state) => {
+  return {
+    contacts: state.contact.contacts
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    getContacts: () =>{
+      dispatch({
+        type: 'GET_CONTACTS'
+      })
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Contacts);
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,11 +1,35 @@
-const initialState = {};
+const initialState = {
+    contacts:[
+        {
+            id: 1,
+            name: 'Mohamed IDBRAHIM',
+            email: 'idbrahimdev@gmail.com',
+            phone: '0650303315'
+          },
+          {
+            id: 2,
+            name: 'Basma IDBRAHIM',
+            email: 'basma@gmail.com',
+            phone: '0650303316'
+          },
+          {
+            id: 3,
+            name: 'Walid IDBRAHIM',
+            email: 'walid@gmail.com',
+            phone: '0650303317'
+          }
+    ]
+};
 
 // traiter les actions pour les contacts!
 export default function (state=initialState, action) {
     
     // verifier l'action subie
     switch(action.type){
-        
+        case 'GET_CONTACTS':
+            return{
+                ...state
+            }
         default: {
             return state;
         }
